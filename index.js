1ebbde044f9cd86baae76235ef29b803518f4d87
Danilo Alonso <damusix@gmail.com>
2017-01-30 18:22:41 -0500

    CLI tool working

--- index.js
+++ index.js
@@ -7,87 +7,67 @@ const fs = require('fs');
 const chalk = require('chalk');
 const program = require('commander');
 const prompt = require('prompt');
+const async = require('async');
 
 const Package = require('./package.json');
 const copy = require('./lib/copy');
 
 let cwd = process.cwd();
-let pictorBase = `${__dirname}/src/base`;
-let pictorScss = `${__dirname}/src/app.scss`;
+let pictorFolder = `${__dirname}/src/base`;
+let pictorFile = `${__dirname}/src/pictor.scss`;
 
-let copyTo;
-let filename;
-let dirname;
-let overwrite;
+const cp = {
+    to: null,
+    filename: null,
+    dirname: null,
+    overwrite: null,
+}
+
+
+/**
+ * Copies Pictor files to current location
+ * @param {String} which - 'file' or 'folder'
+ * @param {Function} callback - Success callback
+ * @param {Function} error - Error callback
+ */
+function copyFile () {
+
+    const dest = {
+        file: path.resolve(cp.to, cp.filename),
+        folder: path.resolve(cp.to, cp.dirname)
+    };
+
+    const src = {
+        file: pictorFile,
+        folder: pictorFolder
+    }
+
+    copy(src['file'], dest['file']);
+    copy(src['folder'], dest['folder']);
+
+}
+
 
 program
     .version(Package.version)
-    // .command('*')
     .option('-f, --filename <filename>', 'name of SCSS entry file. Should not exist. Default is "app.scss"')
     .option('-d, --dirname <dirname>', 'name of base directory. Should not exist. Default is "base"')
     .option('-o, --overwrite [bool]', 'overwrite files if they already exist', parseInt)
     .description('Copy Pictor files to path')
     .action((copyPath, options) => {
 
-        copyTo = copyPath;
-        filename = options.filename || 'app.scss';
-        dirname = options.dirname || 'base';
-        overwrite = options.overwrite || null;
+        Object.assign(cp, {
+
+            to: path.resolve(copyPath),
+            filename: options.filename || 'app.scss',
+            dirname: options.dirname || 'base',
+            overwrite: options.overwrite || null
+
+        });
+
+        copyFile();
+        console.log(chalk.blue('Pictor finished.'));
 
     });
-
-
-copyPath = path.resolve(copyPath);
-const copyPathExists = fs.existsSync(copyTo);
-const copyPathIsDirectory = fs.lstatSync(copyTo).isDirectory();
-
-const sassDir = path.resolve(copyTo, dirname);
-const sassDirExists = fs.existsSync(sassDir);
-const sassDirIsDirectory = fs.lstatSync(sassDir).isDirectory();
-
-const sassFile = path.resolve(copyTo, filename);
-const sassFileExists = fs.existsSync(sassFile);
-const sassFileIsFile = fs.lstatSync(sassFile).isFile();
-
-if (!copyTo) {
-    console.log(chalk.red(copyTo, 'does not exists'));
-    return 1;
-}
-
-if (copyPathExists && copyPathIsDirectory) {
-
-    if (!sassFileExists || (sassFileExists && !sassFileIsFile)) {
-
-        if (overwrite === null) {
-
-            prompt.start();
-
-            prompt.get(`overwite ${filename}?`, () => {
-
-            });
-
-        }
-
-        copy(pictorScss, sassFile);
-        console.log(chalk.green(`${options.filename} was created`));
-
-    } else {
-        console.log(chalk.red(`${options.filename} already exists`));
-    }
-
-    if (!sassDirExists || (sassDirExists && !sassDirIsDirectory)) {
-
-        copy(pictorBase, copyTo);
-        console.log(chalk.green(`${options.filename} was created`));
-
-    } else {
-        console.log(chalk.red(`${options.dirname} already exists`));
-    }
-
-
-} else {
-    console.log(chalk.red(copyTo, 'does not exists'))
-}
-
 
 program.parse(process.argv);
